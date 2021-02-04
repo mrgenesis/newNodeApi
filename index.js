e3e66271e282434cee4b69fa01025348db88a1b2
Rosa, Genesis <mrgenesis@hotmail.com>
2021-02-04 16:07:29 -0400

    refatoração

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Este quivo é o ponto de partida do aplicativo. Ele deve ser executado tanto em ambiente de desenvolvimento quanto em ambiente de produção.
 //Antes da exucução do programa, é necessário definir as variáveis de ambiente para o correto funcionamento do aplicativo.
-const app = require('./src/init')
+const app = require('./src/start')
   , port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('>>> Running at port ' + port + '...'));
